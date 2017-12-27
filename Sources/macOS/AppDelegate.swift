14c5445708ec7b3bea027cbf2e424e5e5dedddf4
mzp <mzpppp@gmail.com>
2017-12-27 17:23:42 +0900

    :computer: add macos target

---
+++ Sources/macOS/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  HeartVoiceViewer
+//
+//  Created by mzp on 2017/12/27.
+//  Copyright © 2017 mzp. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+}
---
+++ Sources/macOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HeartVoiceViewer
+//
+//  Created by mzp on 2017/12/27.
+//  Copyright © 2017 mzp. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+}
