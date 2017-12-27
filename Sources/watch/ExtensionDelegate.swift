be60ddd36f30eb78fbe70570e26c59e615b207dd
mzp <mzpppp@gmail.com>
2017-12-27 17:16:40 +0900

    :watch: add watchkit app

---
+++ Sources/watch/ExtensionDelegate.swift
@@ -0,0 +1,36 @@
+//
+//  ExtensionDelegate.swift
+//  HeartVoiceWatch Extension
+//
+//  Created by mzp on 2017/12/27.
+//  Copyright © 2017 mzp. All rights reserved.
+//
+
+import WatchKit
+
+class ExtensionDelegate: NSObject, WKExtensionDelegate {
+     func handle(_ backgroundTasks: Set<WKRefreshBackgroundTask>) {
+         for task in backgroundTasks {
+            // Use a switch statement to check the task type
+            switch task {
+            case let backgroundTask as WKApplicationRefreshBackgroundTask:
+                // Be sure to complete the background task once you’re done.
+                backgroundTask.setTaskCompletedWithSnapshot(false)
+            case let snapshotTask as WKSnapshotRefreshBackgroundTask:
+                // Snapshot tasks have a unique completion call, make sure to set your expiration date
+                snapshotTask.setTaskCompleted(restoredDefaultState: true,
+                                              estimatedSnapshotExpiration: Date.distantFuture, userInfo: nil)
+            case let connectivityTask as WKWatchConnectivityRefreshBackgroundTask:
+                // Be sure to complete the connectivity task once you’re done.
+                connectivityTask.setTaskCompletedWithSnapshot(false)
+            case let urlSessionTask as WKURLSessionRefreshBackgroundTask:
+                // Be sure to complete the URL session task once you’re done.
+                urlSessionTask.setTaskCompletedWithSnapshot(false)
+            default:
+                // make sure to complete unhandled task types
+                task.setTaskCompletedWithSnapshot(false)
+            }
+        }
+    }
+
+}
