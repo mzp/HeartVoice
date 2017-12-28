471e5573fb9ce53825447ec0106a9b227162567b
mzp <mzpppp@gmail.com>
2017-12-28 12:58:56 +0900

    :computer: run heartvoice service server

--- Sources/macOS/ViewController.swift
+++ Sources/macOS/ViewController.swift
@@ -7,13 +7,32 @@
 //
 
 import Cocoa
+import NorthLayout
 
 class ViewController: NSViewController {
+    let button = NSButton()
+    let heartrate = NSTextField()
+    var server: HeartVoiceServiceServer?
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        button.title = "become a server"
+        button.target = self
+        button.action = #selector(becomeAServer(_:))
+
+        heartrate.isEditable = false
+        heartrate.stringValue = "-"
+
+        let autolayout = view.northLayoutFormat(["p": 20], [
+            "server": button,
+            "heartrate": heartrate
+            ])
+        autolayout("H:|-p-[server]-p-|")
+        autolayout("H:|-p-[heartrate]-p-|")
+        autolayout("V:[server]-p-|")
+        autolayout("V:[heartrate]-p-[server]-p-|")
+
     }
 
     override var representedObject: Any? {
@@ -22,4 +41,26 @@ class ViewController: NSViewController {
         }
     }
 
+    @objc private func becomeAServer(_ sender: AnyObject?) {
+        server = HeartVoiceServiceServer(name: ProcessInfo().hostName, onStateChange: onServerStateChange)
+
+        server?.onActivity = { activity in
+            DispatchQueue.main.async {
+                self.heartrate.stringValue = "💓\(activity.heartrate)"
+            }
+        }
+        server?.start()
+        NSLog("%@", "starting server \(server)")
+        button.title = "Server Started"
+        button.isEnabled = false
+    }
+
+    private func onServerStateChange() {
+        if let server = server {
+            button.title = "\(server.peers.count) peers connected"
+        } else {
+            button.title = "become a server"
+            button.isEnabled = true
+        }
+    }
 }
